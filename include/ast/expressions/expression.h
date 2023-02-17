37e44455628ad997980722a8fde6d9766169e8e4
Ntwali B <ntwali.bashige@gmail.com>
2023-02-17 20:59:11 +0100

    First steps towards handling expressions.

---
+++ include/ast/expressions/expression.h
@@ -0,0 +1,48 @@
+/*  This file is part of the Proto programming language
+ * 
+ *  Copyright (c) 2023- Ntwali Bashige Toussaint
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef PROTO_AST_EXPRESSION_H
+#define PROTO_AST_EXPRESSION_H
+
+enum class ExpressionType {
+    // Primary expressions
+    Literal,
+    // Array,
+    Variable,
+    // FunctionCall,
+    // Group
+};
+
+
+class Expression
+{
+    public:
+        Expression(enum ExpressionType type) : type(type) {}
+
+        /**
+         * Returns the type of this expression.
+         */
+        enum ExpressionType getType() const
+        {
+            return type;
+        }
+
+    protected:
+        enum ExpressionType type;   /* The type of expression of the derived class. */
+};
+
+#endif
---
+++ src/ast/expressions/variable.cc
@@ -0,0 +1,37 @@
+/*  This file is part of the Proto programming language
+ * 
+ *  Copyright (c) 2023- Ntwali Bashige Toussaint
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "ast/expressions/expression.h"
+#include "ast/expressions/variable.h"
+#include "common/token.h"
+
+
+VariableExpression::VariableExpression(
+    Token& token
+) : Expression(ExpressionType::Variable),
+    token(token)
+{}
+
+
+/**
+ * Returns the token associated with this variable definition.
+ */
+Token&
+VariableExpression::getToken()
+{
+    return token;
+}
